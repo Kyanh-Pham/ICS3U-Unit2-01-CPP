eded0e8ed7630b1860407685f075f572aa009262
Kyanh Pham <kyanh.pham@mths.ca>
2022-09-22 15:23:56 +0000

    calculates area and perimeter of circle

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Kyanh Pham//
+// Created by: Kyanh Pham
+// Created on: Sept 2022
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "If a circle has a radius of 15 mm: " << std::endl;
+    std::cout << std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << " mm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * M_PI * 15) << " mm." << std::endl;
+
+    std::cout << "\nDone." << std::endl;
+}
